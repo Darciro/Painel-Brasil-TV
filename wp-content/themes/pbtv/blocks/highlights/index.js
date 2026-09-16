f6f8bdb4e8edc438cfdd91a0df4506ec9247ed1a
Ricardo Carvalho <rickmanu@gmail.com>
2026-09-16 09:12:04 -0300

    Latest news and highlighs blocks configuration and responsive max width

--- wp-content/themes/pbtv/blocks/highlights/index.js
+++ wp-content/themes/pbtv/blocks/highlights/index.js
@@ -1,23 +1,57 @@
-( function ( blocks, element, i18n, ServerSideRender ) {
+( function ( blocks, blockEditor, components, element, i18n, ServerSideRender ) {
 	var registerBlockType = blocks.registerBlockType;
+	var InspectorControls = blockEditor.InspectorControls;
+	var PanelBody = components.PanelBody;
+	var TextControl = components.TextControl;
+	var TextareaControl = components.TextareaControl;
 	var el = element.createElement;
+	var Fragment = element.Fragment;
 	var __ = i18n.__;
 
 	registerBlockType( 'pbtv/highlights', {
 		edit: function ( props ) {
 			var attributes = props.attributes;
-			var videos = attributes.videos || [];
+			var setAttributes = props.setAttributes;
+			var videos = Array.isArray( attributes.videos ) ? attributes.videos : [];
 
-			return videos.filter( Boolean ).length
-				? el( ServerSideRender, {
-						block: 'pbtv/highlights',
-						attributes: attributes,
-				  } )
-				: el(
-						'p',
-						null,
-						__( 'Add one or more YouTube videos to the block attributes to mark them as highlights.', 'pbtv' )
-				  );
+			return el(
+				Fragment,
+				null,
+				el(
+					InspectorControls,
+					null,
+					el(
+						PanelBody,
+						{ title: __( 'Highlights Settings', 'pbtv' ) },
+						el( TextControl, {
+							label: __( 'Heading', 'pbtv' ),
+							value: attributes.heading,
+							onChange: function ( value ) {
+								setAttributes( { heading: value } );
+							},
+						} ),
+						el( TextareaControl, {
+							label: __( 'YouTube video URLs or IDs', 'pbtv' ),
+							help: __( 'One video per line.', 'pbtv' ),
+							value: videos.join( '\n' ),
+							onChange: function ( value ) {
+								setAttributes( {
+									videos: value
+										.split( '\n' )
+										.map( function ( line ) {
+											return line.trim();
+										} )
+										.filter( Boolean ),
+								} );
+							},
+						} )
+					)
+				),
+				el( ServerSideRender, {
+					block: 'pbtv/highlights',
+					attributes: attributes,
+				} )
+			);
 		},
 		save: function () {
 			return null;
@@ -25,6 +59,8 @@
 	} );
 } )(
 	window.wp.blocks,
+	window.wp.blockEditor,
+	window.wp.components,
 	window.wp.element,
 	window.wp.i18n,
 	window.wp.serverSideRender
